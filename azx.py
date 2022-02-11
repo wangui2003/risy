528f001f0bd359072e06e5005b32487497ff73af
wangui2003 <99465604+wangui2003@users.noreply.github.com>
2022-02-11 12:41:58 +0300

    Add files via upload

---
+++ azx.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#taking kilometer input from the user
+kilometer =float(input("enter value in kilometer"))
+#conversion factor
+conv_fac= 0.621371
+#calculate nmiles
+miles=kilometer *conv_fac
+print('% 0.2f (kilometer is equal to % 0.2f miles' % (kilometer, miles))
+
+from time it import default_timer as timer
+f=gm1m2/r2=ma
+gm/r2=g
+m=gr2/g
+m=(9.8m/s2)(6.37*106m)2/(6.673 *10-11 nm2/kg2)
+
+start=timer( )
+print(23*2.3)
+end=timer()
+print(end - start)
+
+# In[1]:
+
+
+import streamlit as st
+st.header("'temperature conversion'")
+st.write ("'**Change Celcius to Fahrenheit**'")
+number = st.number_input("enter the value of temperature in Celcius")
+C = ((number * (9/5))+32)
+st.write (C,"°F")
+
+
+# In[ ]:
+
+
+
+
